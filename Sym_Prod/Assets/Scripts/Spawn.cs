932f36b8d1af1cbe036d47cbd5d4d8c56fb15d3f
StriletsGlib <7658657856+StriletsGlib@users.noreply.github.com>
2020-05-04 18:07:40 +0300

    Test

---
+++ Sym_Prod/Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cellBody;
+    float randX, randY;
+    Vector2 spawnPlace;
+    public float spawnRate = 2;
+    public float borderY, borderX;
+    float nextSpawn = 0;
+    private void Update()
+    {
+        if (Time.time > nextSpawn) {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-borderX, borderX);
+            randY = Random.Range(-borderY, borderY);
+            spawnPlace = new Vector2(randX, randY);
+            Instantiate(cellBody, spawnPlace, Quaternion.identity);
+           
+        }
+    }
+}
---
+++ Sym_Prod/Assets/starting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starting : MonoBehaviour
+{
+    int a = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (a < 1000) { 
+            a = a+1;
+            if (a > 999) { Debug.Log("AAAAA"); }
+        }
+    }
+}
