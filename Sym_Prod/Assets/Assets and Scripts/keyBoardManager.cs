d13f88ab7114e6b32b54142aed47158a92a0e654
StriletsGlib <58534572+StriletsGlib@users.noreply.github.com>
2020-05-15 16:33:53 +0300

    Update keyBoardManager.cs

--- Sym_Prod/Assets/Assets and Scripts/keyBoardManager.cs
+++ Sym_Prod/Assets/Assets and Scripts/keyBoardManager.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class keyBoardManager : MonoBehaviour
 {
+    Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -16,6 +17,12 @@ public class keyBoardManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Escape)){
             SceneManager.LoadScene("Menu");
+        }
+        if(Input.GetKeyDown(KeyCode.Plus)){
+            camera.orthographicSize +=0.5f;
+        }
+        if(Input.GetKeyDown(KeyCode.Minus)){
+            camera.orthographicSize -=0.5f;
         }
     }
 }
